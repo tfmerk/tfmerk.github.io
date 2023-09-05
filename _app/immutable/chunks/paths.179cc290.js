8b9d1f1b53dab587d6e05fece63cff352c480d9c
Thomas Merk <t.f.merk@web.de>
2023-09-05 17:11:24 +0200

    new build, contains the following PRs: https://github.com/tfmerk/tfmerk.github.io/pull/1 and https://github.com/tfmerk/tfmerk.github.io/pull/2

---
+++ _app/immutable/chunks/paths.179cc290.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1g1onp0)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_1g1onp0)==null?void 0:e.assets)??t;export{a,t as b};
