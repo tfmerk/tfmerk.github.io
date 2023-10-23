bc1f967a51f008daa2e8f8476076810f0d04d685
Thomas Merk <t.f.merk@web.de>
2023-10-23 17:16:04 +0200

    https://github.com/tfmerk/tfmerk.github.io/pull/11 and https://github.com/tfmerk/tfmerk.github.io/pull/12

--- _app/immutable/chunks/paths.179cc290.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1g1onp0)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_1g1onp0)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/chunks/paths.ab951c87.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1883j9b)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_1883j9b)==null?void 0:e.assets)??t;export{a,t as b};
